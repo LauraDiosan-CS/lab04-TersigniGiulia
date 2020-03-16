d27617ac77a14841ebf06809e33266892176306c
TersigniGiulia <62182439+TersigniGiulia@users.noreply.github.com>
2020-03-16 18:19:25 +0100

    Add files via upload

---
+++ TesteController.h
@@ -0,0 +1,12 @@
+#pragma once
+#pragma once
+class TesteController
+{
+public:
+	void run();
+	void getSize_isSet();
+	void geSize_isNotSet();
+	void getLength_isSet();
+	void getLength_isNotSet();
+};
+
